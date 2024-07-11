64c4acd9bb74ccee28e62a8c29d03fef7ba67ea8
mauricio <maumarcastil@gmail.com>
2024-07-11 17:52:45 -0500

    feat: Add ThemeProvider to main.tsx for consistent styling across the app

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { ThemeProvider } from '@material-tailwind/react';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
