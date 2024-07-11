f07f9669e9f58e6fa6b4736c64180f7005601e0e
mauricio <maumarcastil@gmail.com>
2024-07-11 17:52:55 -0500

    refactor: Update App.tsx imports and remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
-import ViewMessage from './pages/ViewMessage';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -29,10 +27,12 @@ import '@ionic/react/css/display.css';
 
 /* import '@ionic/react/css/palettes/dark.always.css'; */
 /* import '@ionic/react/css/palettes/dark.class.css'; */
-import '@ionic/react/css/palettes/dark.system.css';
+/* import '@ionic/react/css/palettes/dark.system.css'; */
 
 /* Theme variables */
-import './theme/variables.css';
+import './modules/ui/theme/variables.css';
+
+/* Router */
 import DashboardRouter from './modules/ui/router/dashboardRouter';
 
 setupIonicReact();
